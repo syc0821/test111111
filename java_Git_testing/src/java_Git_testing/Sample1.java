de7a94fbf903e60992391ec3fc096e496a191189
syc0821 <syc0821@naver.com>
2019-04-17 15:47:55 +0900

    commit 4

--- java_Git_testing/src/java_Git_testing/Sample1.java
+++ java_Git_testing/src/java_Git_testing/Sample1.java
@@ -5,5 +5,8 @@ public class Sample1 {
 	System.out.println("안녕하세요!!!! 테스트 중입니다.");
 	System.out.println("안녕하세요!!!! 테스트 중입니다22222.");
 	System.out.println("안녕하세요!!!!!! 테스트 중입니다.333333");
+	System.out.println("안녕하세요!!!!!! 테스트 중입니다.4444");
+	System.out.println("안녕하세요 !! 테스트 중55");
+	
  }//main
 }//end class
