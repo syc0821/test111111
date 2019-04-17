d20ec85601355bf80f37bfd05f7795203ff8eef9
syc0821 <syc0821@naver.com>
2019-04-17 15:38:21 +0900

    commit3

--- java_Git_testing/src/java_Git_testing/Sample1.java
+++ java_Git_testing/src/java_Git_testing/Sample1.java
@@ -3,7 +3,6 @@ package java_Git_testing;
 public class Sample1 {
  public static void main(String[] args) {
 	System.out.println("안녕하세요!!!! 테스트 중입니다.");
-	System.out.println("안녕하세요!!!! 테스트 중입니다.");
-
-}
-}
+	System.out.println("안녕하세요!!!! 테스트 중입니다22222.");
+ }//main
+}//end class
